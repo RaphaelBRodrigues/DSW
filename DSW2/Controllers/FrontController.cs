5c17eeede60a19e99db6031ab45a378c993b8894
Raphael Barbosa Rodrigues <42044496+RaphaelBRodrigues@users.noreply.github.com>
2019-05-22 16:26:47 -0300

    Add files via upload

---
+++ DSW2/Controllers/FrontController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DSW2.Controllers
+{
+    public class FrontController : Controller
+    {
+        // GET: Front
+        public ActionResult Default()
+        {
+            return View();
+        }
+        public ActionResult Guia2()
+        {
+            return View();
+        }
+        public ActionResult Guia3()
+        {
+            return View();
+        }
+        public ActionResult Guia4()
+        {
+            return View();
+        }
+    }
+}
