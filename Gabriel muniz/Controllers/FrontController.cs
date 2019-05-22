6904cdd440670fc17fcc1b1d24078aa0c16c4769
Raphael Barbosa Rodrigues <42044496+RaphaelBRodrigues@users.noreply.github.com>
2019-05-22 16:28:21 -0300

    Add files via upload

---
+++ Gabriel muniz/Controllers/FrontController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gabriel_muniz.Controllers
+{
+    public class FrontController : Controller
+    {
+        // GET: Front
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+        public ActionResult Default()
+        {
+            return View();
+        }
+        public ActionResult pg2()
+        {
+            return View();
+        }
+        public ActionResult p3()
+        {
+            return View();
+        }
+    }
+}
