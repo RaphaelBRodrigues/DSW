d8b05977c64c614c078cac3d84b44ed5a1fd560d
Raphael Barbosa Rodrigues <42044496+RaphaelBRodrigues@users.noreply.github.com>
2019-05-22 15:06:43 -0300

    Add files via upload

---
+++ MasterPiece/Controllers/FrontController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MasterPiece.Controllers
+{
+    public class FrontController : Controller
+    {
+        // GET: Front
+        public ActionResult Default()
+        {
+            return View();
+        }
+    }
+}
